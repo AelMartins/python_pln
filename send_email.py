41863bbaaa9f4b39dd251776f07abb87923b4162
Ael Martins <oneapcwork@gmail.com>
2024-06-26 21:49:12 -0300

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -4,7 +4,7 @@ from email.mime.text import MIMEText
 
 def send_email(subject, body, to_email):
     from_email = 'samuelluizmartinsdossantos@gmail.com'
-    from_password = 'yqfv pvuz kxoa rrjj'
+    from_password = 'yqfvpvuzkxoarrjj'
 
     # Configuração do servidor SMTP
     server = smtplib.SMTP('smtp.gmail.com', 465)
